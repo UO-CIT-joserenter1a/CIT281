4f4fdc386a3aee52a6762009d31046bf2d5036c1
joserenter1a <josevt209@gmail.com>
2023-04-18 10:18:29 -0700

    First commit

--- P2/refactor/p2-random.js
+++ P2/refactor/p2-random.js
@@ -1,11 +1,9 @@
 /*
-	CIT 281 Project 1
+	CIT 281 Project 2
 	Name: Jose Renteria
 */
 
 // lowercase chars
-const chars = "abcdefghijklmnopqrstuvwxyz";
-
 
 // helper function to generate random int
 function getRandomInteger(min, max)
@@ -13,13 +11,17 @@ function getRandomInteger(min, max)
 	return Math.floor(Math.random() * (max - min) + min);
 }
 
-let emp = ""; // empty string to accumulate to
-
-let len = getRandomInteger(5, 26) // random length between 5 (inclusive) and 26 (exclusive)
-
-for(let i = 0; i < len; i ++) // for loop structure 
-{
-	emp += chars[getRandomInteger(0, 25)]; // accumulate to our empty string by generating a random index in our lowercase chars str
+ // empty string to accumulate to
+ // random length between 5 (inclusive) and 26 (exclusive)
+function returnRandomString()
+ {
+	let emp = "";
+	const chars = "abcdefghijklmnopqrstuvwxyz";
+	for(let i = 0; i < getRandomInteger(5, 26); i ++) // for loop structure 
+	{
+		emp += chars[getRandomInteger(0, 25)]; // accumulate to our empty string by generating a random index in our lowercase chars str	
+	}
+	return emp;
 }
 
-console.log(emp);
+console.log(returnRandomString())
