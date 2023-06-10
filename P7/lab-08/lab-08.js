8a62cef566c943a5650a7f4d7d6db0ee16f9ef49
joserenter1a <josevt209@gmail.com>
2023-06-10 16:39:59 -0700

    Lab8

--- P7/lab-08/lab-08.js
+++ P7/lab-08/lab-08.js
@@ -5,20 +5,15 @@ const fetch = require("node-fetch");
 
 // https://jsonplaceholder.typicode.com/photos
 
-fastify.get("/photos/", (request, reply) => {
-    // #3 TODO:
-    // Adapt the following code to attempt to retrieve
-    // all photos from JSONPlaceholder site
-    // using fetch, and handle returned Promise using:
-    // - two .then() chain methods, return 200
-    // - single .catch() chain method, return 404
+fastify.get("/photos", (request, reply) => {
 
-    fetch("https://jsonplaceholder.typicode.com/photos/")
+    fetch("https://jsonplaceholder.typicode.com/photos")
         .then((res) => res.json()) // transforms data 
         .then((resp_json) => 
         {
             // fastify reply
-            console.log(resp_json);
+
+            // console.log(resp_json);
             reply
                 .code(200)
                 .header("Content-Type", "text/json; charset=utf-8")
@@ -32,36 +27,38 @@ fastify.get("/photos/", (request, reply) => {
                 .send({ error: "Route not found", statusCode: 404, photos: [] });
 
             })
-
-
-
-        
   });
   
-  fastify.get("/photos/:id", (request, reply) => {
-    // #4 TODO:
-    // Adapt the following code to attempt to retrieve
-    // a single photo from JSONPlaceholder site
-    // using fetch, and handle returned Promise using:
-    // - single .then() chain method, return 200
-    // - single .catch() chain method, return 404
-    // You may also try to use Object.keys() to 
-    // ensure JSONPlaceholder returns an object with
-    // properties. An empty object returned from 
-    // JSONPlaceholder means that the passed photo ID
-    // was invalid. Your server would also return
-    // a 404 status code for an invalid Photo ID.
-  
-    const { id = "" } = request.params;  
+
+
+fastify.get("/photos/:id", (request, reply) => {
+  const { id = "" } = request.params.id;
+
+    fetch("https://jsonplaceholder.typicode.com/photos" + id)
+    .then((response) => response.json())
+    .then((photo) => {
+      reply
+      .code(200)
+      .header("Content-Type", "application/json; charset=utf-8")
+      .send({ error: "", statusCode: 200, photos: id});
+    })
+    .catch((error) => {
+      reply
+      .code(200)
+      .header("Content-Type", "text/json; charset=utf-8")
+      .send({ error: "Error", statusCode: 404, photos: []});
+    });
+    
     reply
-    .code(200)
-    .header("Content-Type", "text/json; charset=utf-8")
-    .send({ error: "", statusCode: 200, photo: {} });
+      .code(200)
+      .header("Content-Type", "text/json; charset=utf-8")
+      .send({ error: "ERROR", statusCode: 404 });
   });
+
   
   // Start server and listen to requests using Fastify
   const listenIP = "localhost";
-  const listenPort = 8082;
+  const listenPort = 8080;
   fastify.listen(listenPort, listenIP, (err, address) => {
     if (err) {
       console.log(err);
